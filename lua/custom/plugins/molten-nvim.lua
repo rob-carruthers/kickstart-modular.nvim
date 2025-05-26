fd01bb918064ba2e837645875d5bf4f54810ea6b
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-05-26 09:55:31 +0100

    Added molten keymaps

--- lua/custom/plugins/molten-nvim.lua
+++ lua/custom/plugins/molten-nvim.lua
@@ -5,6 +5,14 @@ return {
     build = ':UpdateRemotePlugins',
     init = function()
       vim.g.molten_output_win_max_height = 12
+      vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialise Molten' })
+      vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'Run operator selection' })
+      vim.keymap.set('n', '<localleader>rl', ':MoltenEvaluateLine<CR>', { silent = true, desc = 'Evaluate line' })
+      vim.keymap.set('n', '<localleader>rr', ':MoltenReevaluateCell<CR>', { silent = true, desc = 'Re-evaluate cell' })
+      vim.keymap.set('v', '<localleader>r', ':<C-u>MoltenEvaluateVisual<CR>gv', { silent = true, desc = 'Evaluate visual selection' })
+      vim.keymap.set('n', '<localleader>rd', ':MoltenDelete<CR>', { silent = true, desc = 'Delete cell' })
+      vim.keymap.set('n', '<localleader>oh', ':MoltenHideOutput<CR>', { silent = true, desc = 'Hide cell output' })
+      vim.keymap.set('n', '<localleader>os', ':noautocmd MoltenEnterOutput<CR>', { silent = true, desc = 'Show/enter output' })
     end,
   },
 }
