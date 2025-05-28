2aa1b4b6a01414a2b7af14333ef0d88f4ee18547
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-05-28 21:24:34 +0100

    force neo-tree to open new buffers, update bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,3 +1,24 @@
 return { -- using lazy.nvim
-  { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons', lazy = false,}
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    lazy = false,
+    opts = {
+      mode = 'buffers',
+      always_show_bufferline = true,
+    },
+    config = function()
+      require('bufferline').setup {
+        options = {
+          mode = 'buffers', -- IMPORTANT: shows buffer tabs, not tabpages
+          always_show_bufferline = true,
+          show_buffer_close_icons = true,
+          diagnostics = false,
+        },
+      }
+      vim.o.showtabline = 2
+    end,
+  },
 }
+-- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -17,9 +17,15 @@ return {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['\\'] = 'close_window', -- your custom close key
+          ['<cr>'] = 'open',       -- default: opens in current window (but still creates buffer)
+          ['s'] = 'open_split',    -- open in horizontal split (new buffer)
+          ['v'] = 'open_vsplit',   -- open in vertical split (new buffer)
+          ['t'] = 'open_tabnew',   -- open in new tab page
         },
       },
+      follow_current_file = { enabled = true }, -- optional but nice
+      hijack_netrw_behavior = 'open_default',   -- recommended
     },
   },
 }
