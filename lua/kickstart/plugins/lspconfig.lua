042e62a6108a9ea3849d4e5492f59f0bab06e312
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-06-03 10:16:53 +0100

    removed changed files watcher for Windows network shares

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -266,6 +266,9 @@ return {
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            server.handlers = vim.tbl_extend('force', server.handlers or {}, {
+              ['workspace/didChangeWatchedFiles'] = function() end,
+            })
             require('lspconfig')[server_name].setup(server)
           end,
         },
