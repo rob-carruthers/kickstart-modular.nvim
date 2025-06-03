6a3af39a47ff7906860321ce66c749327df11803
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-06-03 10:37:38 +0100

    disable git watching

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -74,6 +74,9 @@ return {
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
         map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
       end,
+      watch_gitdir = {
+        follow_files = false,
+      },
     },
   },
 }
