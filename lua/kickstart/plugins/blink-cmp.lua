68be1bbdb8914b60c452072e21e41731471190ad
Rob Carruthers <12486527+rob-carruthers@users.noreply.github.com>
2025-06-20 22:19:02 +0100

    enable borders and documentation showing in blink-cmp

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -72,7 +72,11 @@ return {
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'rounded' } },
+        menu = {
+          border = 'rounded',
+          winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None',
+        },
       },
 
       sources = {
